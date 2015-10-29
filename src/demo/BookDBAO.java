d25accab62b8ff5076c0fe2dfe5d30fe0debf8ce
Jeffry Kong <appleg75@Jeffrys-MacBook-Pro.local>
2015-10-29 17:02:58 +0800

    Initial commit

---
+++ src/demo/BookDBAO.java
@@ -0,0 +1,36 @@
+package demo;
+/**
+ * Created by appleg75 on 10/23/15.
+ */
+public class BookDBAO {
+    // get book details given the bookId
+    // this method is dummy for now
+    public BookDetails getBookDetails(String bookId) {
+        // return dummy book for testing now
+        return createDummyBook(bookId);
+    }
+
+    public BookDetails[] getAllBooks(){
+        BookDetails[] bArray = new BookDetails[10];
+        bArray[1] = getBookDetails("203");
+        return bArray;
+    }
+
+    // convenient method to create a dummy book detail
+    // this method is meant for testing
+    private BookDetails createDummyBook(String bookId) {
+        BookDetails details = new BookDetails();
+        details.setBookId(bookId);
+        details.setDescription("Web components for developers talks about how web components can transfor the way you develop application for the web");
+        details.setFirstName("Java");
+        details.setSurname("Expert");
+        details.setInventory(5);
+        details.setOnSale(false);
+        details.setPrice(5.9f);
+        details.setTitle("Web components for developers");
+        details.setYear(2015);
+        return details;
+    }
+
+
+}
